d4135dd9e8e648d273d66b17e9492f4441b217c5
IshaRode <you@example.com>
2024-01-13 18:18:27 +0530

    Added 3 components

---
+++ hooks-1/src/components/UseState.jsx
@@ -0,0 +1,27 @@
+// To set state and update
+import { useState } from "react";
+
+function UseState() {
+  const [state, setState] = useState(true);
+
+  function handleClick() {
+    setState(!state);
+    console.log(state);
+  }
+
+  const themeStyle = {
+    backgroundColor: state ? "black" : "grey",
+    color: state ? "grey" : "black",
+    padding: "2rem",
+    margin: "2rem",
+  };
+  
+  return (
+    <>
+      <button onClick={handleClick}>Toggle for useState</button>
+      <div style={themeStyle}>This is made by using useState</div>
+    </>
+  );
+}
+
+export default UseState;
